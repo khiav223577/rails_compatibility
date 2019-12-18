0a0a9a2e294e9561f5759cb00d33c68ae707326c
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2019-12-18 17:24:02 +0800

    Fix: Rails 3 doesn't have column_types method

--- lib/rails_compatibility/attribute_types.rb
+++ lib/rails_compatibility/attribute_types.rb
@@ -4,13 +4,17 @@ require 'rails_compatibility'
 require 'active_record'
 
 class << RailsCompatibility
-  if ActiveRecord::Base.respond_to?(:attribute_types)
+  if ActiveRecord::Base.respond_to?(:attribute_types) # column_types was changed to attribute_types in Rails 5
     def attribute_types(klass)
-      klass.attribute_types # column_types was changed to attribute_types in Rails 5
+      klass.attribute_types
     end
-  else
+  elsif ActiveRecord::Base.respond_to?(:column_types) # Rails 4
     def attribute_types(klass)
       klass.column_types
+    end
+  else # In Rails 3
+    def attribute_types(klass)
+      klass.columns_hash
     end
   end
 end
