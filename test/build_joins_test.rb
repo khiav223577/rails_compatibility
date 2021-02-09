62905209c0e8ced263cf5efc8e2e0913eeba607f
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2021-02-09 21:35:08 +0800

    should only join HABTM in test cases

--- test/build_joins_test.rb
+++ test/build_joins_test.rb
@@ -11,23 +11,23 @@ class BuildJoinsTest < Minitest::Test
     reflect = County.reflect_on_association(:zipcodes)
 
     relation = Zipcode.where(zip: '30301')
-    relation = relation.joins(RailsCompatibility.build_joins(reflect, relation))
+    relation = relation.joins(RailsCompatibility.build_joins(reflect, relation)[0])
     assert_equal [@county_ids['Fulton']], relation.pluck('counties_zipcodes.county_id')
 
     relation = Zipcode.where(zip: '30291')
-    relation = relation.joins(RailsCompatibility.build_joins(reflect, relation))
+    relation = relation.joins(RailsCompatibility.build_joins(reflect, relation)[0])
     assert_equal [@county_ids['Fulton']], relation.pluck('counties_zipcodes.county_id')
 
     relation = Zipcode.where(zip: '55410')
-    relation = relation.joins(RailsCompatibility.build_joins(reflect, relation))
+    relation = relation.joins(RailsCompatibility.build_joins(reflect, relation)[0])
     assert_equal [@county_ids['Hennepin']], relation.pluck('counties_zipcodes.county_id')
 
     relation = Zipcode.where(zip: '55416')
-    relation = relation.joins(RailsCompatibility.build_joins(reflect, relation))
+    relation = relation.joins(RailsCompatibility.build_joins(reflect, relation)[0])
     assert_equal [@county_ids['Hennepin']], relation.pluck('counties_zipcodes.county_id')
 
     relation = Zipcode.where(zip: '!INVALID!')
-    relation = relation.joins(RailsCompatibility.build_joins(reflect, relation))
+    relation = relation.joins(RailsCompatibility.build_joins(reflect, relation)[0])
     assert_equal [], relation.pluck('counties_zipcodes.county_id')
   end
 
@@ -35,15 +35,15 @@ class BuildJoinsTest < Minitest::Test
     reflect = Zipcode.reflect_on_association(:counties)
 
     relation = County.where(name: 'Fulton')
-    relation = relation.joins(RailsCompatibility.build_joins(reflect, relation))
+    relation = relation.joins(RailsCompatibility.build_joins(reflect, relation)[0])
     assert_equal [@zipcode_ids['30301'], @zipcode_ids['30291']], relation.pluck('counties_zipcodes.zipcode_id')
 
     relation = County.where(name: 'Hennepin')
-    relation = relation.joins(RailsCompatibility.build_joins(reflect, relation))
+    relation = relation.joins(RailsCompatibility.build_joins(reflect, relation)[0])
     assert_equal [@zipcode_ids['55410'], @zipcode_ids['55416']], relation.pluck('counties_zipcodes.zipcode_id')
 
     relation = County.where(name: '!INVALID!')
-    relation = relation.joins(RailsCompatibility.build_joins(reflect, relation))
+    relation = relation.joins(RailsCompatibility.build_joins(reflect, relation)[0])
     assert_equal [], relation.pluck('counties_zipcodes.zipcode_id')
   end
 end
