ba6759d273fbc967704db41e60a7424c592f63db
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2021-06-11 04:41:10 +0800

    Fix: ActiveRecord::ImmutableRelation

--- lib/rails_compatibility/apply_join_dependency.rb
+++ lib/rails_compatibility/apply_join_dependency.rb
@@ -8,6 +8,14 @@ class << RailsCompatibility
     def apply_join_dependency(relation)
       relation.send(:apply_join_dependency)
     end
+  elsif GTE_RAILS_5_1
+    def apply_join_dependency(relation)
+      relation.send(:construct_relation_for_association_calculations)
+    end
+  elsif GTE_RAILS_5_0
+    def apply_join_dependency(relation)
+      relation.dup.send(:construct_relation_for_association_calculations)
+    end
   else
     def apply_join_dependency(relation)
       relation.send(:construct_relation_for_association_calculations)
